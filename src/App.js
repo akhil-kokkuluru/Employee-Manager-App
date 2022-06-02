0a795b6518f77aecdc1f173e2df65989467d1898
akhil <akhilr37@gmail.com>
2022-06-02 20:21:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import './App.css'
+import TableComponent from './Home'
+
+const App = () => <TableComponent />
+
+export default App
---
+++ src/Home/index.js
@@ -0,0 +1,44 @@
+import {Component} from 'react'
+import {AgGridReact} from 'ag-grid-react'
+
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
+
+class TableComponent extends Component {
+  state = {
+    columnDefs: [
+      {field: 'id'},
+      {field: 'first_name'},
+      {field: 'last_name'},
+      {field: 'email'},
+      {field: 'number'},
+      {field: 'gender'},
+    ],
+    rowData: [],
+  }
+
+  componentDidMount() {
+    this.apicall()
+  }
+
+  apicall = async () => {
+    const response = await fetch(
+      'https://6149991107549f001755a471.mockapi.io/employees/employees',
+    )
+    const data = await response.json()
+    this.setState({rowData: data})
+    console.log(data)
+  }
+
+  render() {
+    const {rowData, columnDefs} = this.state
+    return (
+      <div className="ag-theme-alpine" style={{height: 400, width: 1000}}>
+        <h1>Employee Manager</h1>
+        <AgGridReact rowData={rowData} columnDefs={columnDefs} />
+      </div>
+    )
+  }
+}
+
+export default TableComponent
